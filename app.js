f69b47be3ae95242715fa767df3d02e79f76c9e2
Aleksey Yesin <esinru@gmail.com>
2021-06-05 20:57:31 +0300

    Adds init structure

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const api = require("./api");
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api/v1/contacts", api.contacts);
+
+app.use((_, res) => {
+    res.status(404).json({ message: "Resourse not found" });
+});
+
+app.use((err, _, res, __) => {
+    const { code = 500, message = "Server error" } = err;
+
+    res.status(code).json({ message });
+});
+
+app.listen(3000);
