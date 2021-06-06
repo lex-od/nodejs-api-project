0a3530587f603c8d210d7027dea5db77e420db35
Aleksey Yesin <esinru@gmail.com>
2021-06-06 20:30:41 +0300

    Adds getOne contacts controller

--- controllers/contacts/getOne.js
+++ controllers/contacts/getOne.js
@@ -1,9 +1,22 @@
 const { jsonContacts: db } = require("../../services");
 
-const getOne = async (req, res) => {
-    res.json({
-        result: "will be requested contact",
-    });
+const getOne = async (req, res, next) => {
+    try {
+        const contact = await db.getContactById(req.params.id);
+
+        if (!contact) {
+            const err = new Error("Contact with requested ID does not exist");
+            err.code = 404;
+
+            return next(err);
+        }
+
+        res.json({
+            result: contact,
+        });
+    } catch {
+        next(new Error("Data access error"));
+    }
 };
 
 module.exports = getOne;
