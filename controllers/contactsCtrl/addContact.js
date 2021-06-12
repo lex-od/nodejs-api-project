e6d7e3e6fc75f067e8e0c1b30a5517273dbd4a23
Aleksey Yesin <esinru@gmail.com>
2021-06-12 20:45:15 +0300

    Removes some comments

--- controllers/contactsCtrl/addContact.js
+++ controllers/contactsCtrl/addContact.js
@@ -2,16 +2,10 @@ const { contactsOps: ops } = require("../../services");
 
 const addContact = async ({ body }, res, next) => {
     try {
-        // const { error } = joi.newContact.validate(body);
-
-        // if (error)
-        //     return res
-        //         .status(400)
-        //         .json({ message: error.details?.[0]?.message });
-
         res.status(201).json({ result: await ops.addContact(body) });
     } catch (err) {
         console.log(err.name, err.message);
+        // 📌 Добавить обработчик ошибок валидации
 
         next(new Error("Data access error"));
     }
