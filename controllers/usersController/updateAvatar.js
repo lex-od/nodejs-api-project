8061f01cb4111e21e878a9b73184c6c4fb4a1331
Aleksey Yesin <esinru@gmail.com>
2021-06-24 18:07:57 +0300

    Adds jimp file resizing

--- controllers/usersController/updateAvatar.js
+++ controllers/usersController/updateAvatar.js
@@ -1,11 +1,29 @@
 const jimp = require("jimp");
+const path = require("path");
 const { usersService: srv } = require("../../services");
 // const { ApiError, apiConsts } = require("../../helpers");
 
 // const { DB_ACCESS_ERROR } = apiConsts;
 
-const updateAvatar = async ({ user: { _id }, file }, res, next) => {
+const uploadFolder = path.join(process.cwd(), "public", "avatars");
+
+const updateAvatar = async (
+    { user: { _id }, file: { path: filePath, filename } },
+    res,
+    next
+) => {
     try {
+        const jimpImg = await jimp.read(filePath);
+
+        await jimpImg
+            .autocrop()
+            .cover(
+                250,
+                250,
+                jimp.HORIZONTAL_ALIGN_CENTER | jimp.VERTICAL_ALIGN_MIDDLE
+            )
+            .writeAsync(filePath);
+
         res.json();
     } catch {
         // next(new ApiError(DB_ACCESS_ERROR));
